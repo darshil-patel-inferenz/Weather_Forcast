ead4bbd9bd8f91f15e528c88aa988e3e12f50f42
darshil602 <darshil.patel@inferenz.ai>
2021-12-03 14:14:24 +0530

    time

--- app.js
+++ app.js
@@ -18,11 +18,10 @@ app.get("/", (req, res) => {
   res.send("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 });
 app.post("/postData", (req, res) => {
-  let timestamp = new Date();
   const inputData = {
     Temperature: req.body.Temperature,
     Humidity: req.body.Humidity,
-    TimeStamp:timestamp
+    TimeStamp:req.body.TimeStamp
   };
 
   var sql = "INSERT INTO Weather_Forcast SET ?";
